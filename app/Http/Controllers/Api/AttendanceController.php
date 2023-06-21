6cd9f09aa5656ca372f644b066c5aa7c6c330fab
dieosurya867 <dieosurya@gmail.com>
2023-06-21 15:15:32 +0700

    [add] Api Endpoint Absensi History

--- app/Http/Controllers/Api/AttendanceController.php
+++ app/Http/Controllers/Api/AttendanceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Traits\ImageStorage;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -111,5 +112,28 @@ class AttendanceController extends Controller
                 Response::HTTP_UNPROCESSABLE_ENTITY,
             );
         }
+    }
+
+    public function history(Request $request){
+        $request->validate([
+            'from' => ['required'],
+            'to' => ['required'],
+        ]);
+
+        $history = $request->user()->attendances()->with('detail')
+        ->whereBetween(
+            DB::raw('DATE(created_at)'),
+            [
+                $request->from,$request->to
+            ]
+        )->get();
+        return response()->json([
+            'message' => "list of presences by user",
+            'data' => $history,
+        ],
+        Response::HTTP_OK
+    );
+
+
     }
 }
